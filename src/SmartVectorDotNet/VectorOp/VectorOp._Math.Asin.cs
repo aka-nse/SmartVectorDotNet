6c7f09a47e2000b756d102d52dc576c620953993
aka-nse <nakase.d1220@gmail.com>
2025-01-19 21:25:30 +0900

    fixes formula of VectorOp.Asin (#28)

--- src/SmartVectorDotNet/VectorOp/VectorOp._Math.Asin.cs
+++ src/SmartVectorDotNet/VectorOp/VectorOp._Math.Asin.cs
@@ -16,5 +16,5 @@ partial class VectorOp
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector<T> Asin<T>(Vector<T> x)
         where T : unmanaged
-        => Asin_<T>._2 * Atan(x / (x + Sqrt(Asin_<T>._1 - x * x)));
+        => Asin_<T>._2 * Atan(x / (Asin_<T>._1 + Sqrt(Asin_<T>._1 - x * x)));
 }
