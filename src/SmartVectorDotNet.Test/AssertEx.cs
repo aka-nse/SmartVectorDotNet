91b759b11ec4d13f94cfc4a50a85f411f113d046
unknown <nakase.d1220@gmail.com>
2025-04-13 19:37:18 +0900

    adds test

--- src/SmartVectorDotNet.Test/AssertEx.cs
+++ src/SmartVectorDotNet.Test/AssertEx.cs
@@ -21,4 +21,52 @@ public static class AssertEx
             Assert.Fail($"{message}\r\n  expected: {expected}\r\n  acutual: {actual}");
         }
     }
+
+    [DebuggerHidden]
+#if NET6_0_OR_GREATER
+    [StackTraceHidden]
+#endif
+    public static void SameBehavior<T>(Func<T> expected, Func<T> actual)
+    {
+        bool expectedIsFailed = false;
+        bool actualIsFailed = false;
+        T expectedResult = default!;
+        T actualResult = default!;
+        Exception expectedError = default!;
+        Exception actualError = default!;
+        try
+        {
+            expectedResult = expected();
+        }
+        catch(Exception exc)
+        {
+            expectedIsFailed = true;
+            expectedError = exc;
+        }
+        try
+        {
+            actualResult = actual();
+        }
+        catch (Exception exc)
+        {
+            actualIsFailed = true;
+            actualError = exc;
+        }
+
+        switch((expectedIsFailed, actualIsFailed))
+        {
+        case (true, false):
+            Assert.Fail($"Expected to fail but succeeded.\r\nexpected error: {expectedError.GetType()}");
+            return;
+        case (false, true):
+            Assert.Fail($"Expected to succeed but failed.\r\nexpected value: {expectedResult}");
+            return;
+        case (true, true):
+            Assert.Equal(expectedError.GetType(), actualError.GetType());
+            return;
+        case (false, false):
+            Assert.Equal(expectedResult, actualResult);
+            return;
+        }
+    }
 }
