dd5b0ae605c3eb03d2025fa395cc9db0c79ffb09
unknown <nakase.d1220@gmail.com>
2025-06-29 16:45:26 +0900

    prepares basis

---
+++ src/SmartVectorDotNet.Generators/ImplicitFormulaGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis;
+
+namespace SmartVectorDotNet.Generators;
+
+[Generator(LanguageNames.CSharp)]
+public partial class ImplicitFormulaGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        context.RegisterPostInitializationOutput(static cxt =>
+        {
+            cxt.AddSource("Vectorization.VectorizationFormulaExtension.g.cs", VectorizationFormulaExtension);
+        });
+    }
+}
