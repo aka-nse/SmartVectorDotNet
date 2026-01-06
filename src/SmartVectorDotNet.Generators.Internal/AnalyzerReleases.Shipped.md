7f7fd18e0a05d4e400df686558af2d95bc91159c
aka-nse <nakase.d1220@gmail.com>
2026-01-07 00:22:49 +0900

    adds new analyzer to detect unsafe API calls

---
+++ src/SmartVectorDotNet.Generators.Internal/AnalyzerReleases.Shipped.md
@@ -0,0 +1,7 @@
+# Release 0.1.2.0
+
+## New Rules
+
+Rule ID   | Category | Severity | Notes
+----------|----------|----------|------------
+SVINT1001 | Security | Error    | Unsafe APIs are required to be used within an `unsafe` context.
---
+++ src/SmartVectorDotNet.Generators.Internal/UnsafeContextAnalyzer.cs
@@ -0,0 +1,69 @@
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace SmartVectorDotNet.Generators.Internal;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+internal class UnsafeContextAnalyzer : DiagnosticAnalyzer
+{
+    private static readonly DiagnosticDescriptor _rule = new(
+        $"{GlobalConstants.DiagnosticIDBase}1001",
+        "Unsafe context required",
+        "Unsafe API `{0}` is required to be used within an `unsafe` context",
+        "Security",
+        DiagnosticSeverity.Warning,
+        isEnabledByDefault: true);
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } =
+        [_rule];
+
+    public override void Initialize(AnalysisContext context)
+    {
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+        context.EnableConcurrentExecution();
+        context.RegisterSyntaxNodeAction(AnalyzeUnsafeApiCall,
+            SyntaxKind.SimpleMemberAccessExpression);
+    }
+
+    private static void AnalyzeUnsafeApiCall(SyntaxNodeAnalysisContext context)
+    {
+        var member = context.SemanticModel.GetSymbolInfo(context.Node).Symbol;
+        if (member?.ContainingType is not { } typeSymbol)
+        {
+            return;
+        }
+
+        switch (typeSymbol.ToDisplayString())
+        {
+        case "System.Runtime.CompilerServices.Unsafe":
+        case "System.Runtime.InteropServices.MemoryMarshal":
+            break;
+        default:
+            // Not an unsafe API
+            return;
+        }
+
+        foreach(var ancestor in context.Node.Ancestors())
+        {
+            if (ancestor is UnsafeStatementSyntax)
+            {
+                // Already in an unsafe context
+                return;
+            }
+            if(ancestor is MethodDeclarationSyntax methodDeclaration
+                && methodDeclaration.ChildTokens().Any(static token => token.IsKind(SyntaxKind.UnsafeKeyword)))
+            {
+                // Already in an unsafe context
+                return;
+            }
+        }
+
+        context.ReportDiagnostic(Diagnostic.Create(
+            _rule,
+            context.Node.GetLocation(),
+            member.ToDisplayString()));
+    }
+}
