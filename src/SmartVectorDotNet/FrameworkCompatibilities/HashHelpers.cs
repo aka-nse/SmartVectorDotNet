b727dc314c481962cfc3b684b09d8d9cc3d9bcde
Daishi Nakase <nakase.d1220@gmail.com>
2026-01-12 21:42:18 +0900

    fixes linebreak

--- src/SmartVectorDotNet/FrameworkCompatibilities/HashHelpers.cs
+++ src/SmartVectorDotNet/FrameworkCompatibilities/HashHelpers.cs
@@ -7,7 +7,9 @@
 
 using System.Diagnostics.CodeAnalysis;
 
+#pragma warning disable IDE0130
 namespace System.Numerics.Hashing
+#pragma warning restore IDE0130
 {
     internal static class HashHelpers
     {
